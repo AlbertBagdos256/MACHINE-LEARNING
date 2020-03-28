01c7b59b12c9da0c080145a7ab955cf1dff33ad5
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2020-03-28 23:08:26 +0500

    Number's identifying neural network.
    Potentially you could use instead of number a=everything which you want,it's up to you.

---
+++ Number's identifying/NeuralNetwork02.py
@@ -0,0 +1,138 @@
+# scipy.special for the sigmoid function expit()
+import scipy.special
+import numpy
+# library for plotting arrays
+import matplotlib.pyplot
+# ensure the plots are inside this notebook, not an external window
+# helper to load data from PNG image files
+import imageio
+from letters import symbols_amount
+# neural network class definition
+class neuralNetwork:
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc 
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
+        # learning rate
+        self.lr = learningrate
+        
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+        
+        pass
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        hidden_errors = numpy.dot(self.who.T, output_errors) 
+        
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+        
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+        
+        pass
+
+
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+        
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+        
+        return final_outputs
+
+# number of input, hidden and output nodes
+input_nodes = 784
+hidden_nodes = 200
+output_nodes = 10
+
+# learning rate
+learning_rate = 10
+
+# create instance of neural network
+n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
+
+# load the mnist training data CSV file into a list
+training_data_file = open("./mnist_dataset/mnist_train_100.csv", 'r')
+training_data_list = training_data_file.readlines()
+
+
+epochs = 10
+
+for e in range(epochs):
+    # go through all records in the training data set
+    for record in training_data_list:
+        # split the record by the ',' commas
+        all_values = record.split(',')
+        # scale and shift the inputs
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # create the target output values (all 0.01, except the desired label which is 0.99)
+        targets = numpy.zeros(output_nodes) + 0.01
+        # all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+        pass
+    pass
+
+
+# test the neural network with our own images
+
+# load image data from png files into an array
+print ("loading ... my_own_images/2828_my_own_image.png")
+img_array = imageio.imread('my_own_images/2828_my_own_image.png', as_gray=True)
+    
+# reshape from 28x28 to list of 784 values, invert values
+img_data  = 255.0 - img_array.reshape(784)
+    
+# then scale data to range from 0.01 to 1.0
+img_data = (img_data / 255.0 * 0.99) + 0.01
+print("min = ", numpy.min(img_data))
+print("max = ", numpy.max(img_data))
+
+# plot image
+matplotlib.pyplot.imshow(img_data.reshape(28,28), cmap='Greys', interpolation='None')
+
+# query the network
+outputs = n.query(img_data)
+print (outputs)
+
+label = numpy.argmax(outputs)
+print("network says ", label)
+
+
+
+training_data_file.close()
