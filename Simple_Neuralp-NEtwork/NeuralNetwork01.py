76fafe59ed601eca745002180184c37b4e68412a
AlbertBagdos256 <44371952+AlbertBagdos256@users.noreply.github.com>
2020-03-28 23:17:28 +0500

    First simple neural-network

---
+++ Simple_Neuralp-NEtwork/NeuralNetwork01.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 * np.exp(-x))
+
+
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [0,0,0],
+                            [1,0,0]])
+                            
+training_outputs = np.array([[0,1,0,1]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3,1)) - 1
+
+print("Random starting synaptic weight")
+print(synaptic_weights)
+
+for i in range(10000):
+    input_layer = training_inputs
+    
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+    
+    err = training_outputs - outputs
+    adjustments = np.dot(input_layer.T,err*(outputs*(1-outputs)))
+    
+    synaptic_weights += adjustments
+    
+print("Synaptic weights after training")
+print(synaptic_weights)
+    
+print("Outpits after training")
+print(outputs)
+
+#TEst
+new_inputs = np.array([1,1,0])
+output = sigmoid(np.dot(new_inputs, synaptic_weights))
+print(output)
